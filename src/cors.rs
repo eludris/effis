e0fdd9affae8f4683e0a233350e238d801995531
Enoki <enokiun@gmail.com>
2022-11-29 02:38:52 +0300

    fix: fix cors

--- src/cors.rs
+++ src/cors.rs
@@ -1,5 +1,5 @@
 use rocket::fairing::{Fairing, Info, Kind};
-use rocket::http::Header;
+use rocket::http::{Header, Method, Status};
 use rocket::{Request, Response};
 
 pub struct Cors;
@@ -13,10 +13,17 @@ impl Fairing for Cors {
         }
     }
 
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, OPTIONS",
+        ));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+
+        if request.method() == Method::Options && response.status() == Status::NotFound {
+            response.set_status(Status::NoContent);
+        }
     }
 }
