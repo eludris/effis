5e771fdc2525260bd5fa8caf3112445067dcb3c9
Enoki <enokiun@gmail.com>
2022-11-30 00:58:15 +0300

    feat: remove the test route

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,10 +1,10 @@
+mod static_routes;
+
 use rocket::Route;
 
-#[get("/")]
-pub async fn index() -> &'static str {
-    "h"
-}
-
 pub fn routes() -> Vec<Route> {
-    routes![index]
+    routes![
+        static_routes::fetch_static_file,
+        static_routes::download_static_file
+    ]
 }
